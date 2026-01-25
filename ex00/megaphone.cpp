910f9aecfe2f9d51050510a16a3b70087874622b
WaroonRagwongsiri <waroonwork@gmail.com>
2026-01-25 17:53:01 +0700

    Forgot gigignore

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: waroonwork@gmail.com <WaroonRagwongsiri    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/25 14:09:02 by waroonwork@       #+#    #+#             */
+/*   Updated: 2026/01/25 14:27:08 by waroonwork@      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(int argc, char **argv)
+{
+	int	i;
+	int	j;
+
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+		return (0);
+	}
+	i = 1;
+	while (i < argc)
+	{
+		j = 0;
+		while (argv[i][j])
+		{
+			argv[i][j] = toupper(argv[i][j]);
+			j++;
+		}
+		std::cout << argv[i];
+		i++;
+	}
+	std::cout << "\n";
+	return (0);
+}
